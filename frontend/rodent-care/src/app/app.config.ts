eff49ce1805cf44a7aed15fa9e7740eb97526db8
Nikson <nikolaking88@gmail.com>
2026-01-24 17:28:01 +0100

    Initialized frontend

--- frontend/rodent-care/src/app/app.config.ts
+++ frontend/rodent-care/src/app/app.config.ts
@@ -2,18 +2,26 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptorsFromDi, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeng/themes/aura';
 
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { AuthInterceptor } from './services/auth/auth-interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideClientHydration(withEventReplay()),
     provideHttpClient(withInterceptorsFromDi()),
     provideAnimationsAsync(),
+    providePrimeNG({
+      theme: {
+        preset: Aura,
+        options: {
+          darkModeSelector: '.dark-mode'
+        }
+      }
+    }),
     {
       provide: HTTP_INTERCEPTORS,
       useClass: AuthInterceptor,
